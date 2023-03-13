ad358b230bc9624a3524d731cf929f379f076b20
nixon <nichofernandez1@gmail.com>
2023-03-13 09:36:46 -0600

    carga del proyecto

---
+++ README.md
@@ -0,0 +1,8 @@
+# Playcr.la
+
+usar la version de node de 14.17.5
+
+npm i
+
+ npm run dev
+ 
---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+define("SITE_NAME","Playcr");
+define("DOMAIN_NAME","Playcr.la");
+define("DGS_SITEID","847");
+
+require "C:\\inetpub\\wwwroot\\generalConfig.php";
+?>

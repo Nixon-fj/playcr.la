ced18bcdd203422cba87d191d059e6b517468518
nixon <nichofernandez1@gmail.com>
2023-03-16 15:51:29 -0600

    cambios generales

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 define("SITE_NAME","Playcr");
 define("DOMAIN_NAME","Playcr.la");
-define("DGS_SITEID","847");
+define("DGS_SITEID","952");
 
 require "C:\\inetpub\\wwwroot\\generalConfig.php";
 ?>
